58f9218067e8525b49e30ab3a6c9673b6ea830b2
Pratik Shastrakar <48235970+pstricks01@users.noreply.github.com>
2022-01-10 12:09:38 +0530

    resume link removed (conflicting with build)

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -10,8 +10,8 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  "https://github.com/pstricks01/pratik-s-portfolio/blob/main/src/Assets/Pratik_Shastrakar_Resume.pdf";
+// const resumeLink =
+//   "https://github.com/pstricks01/pratik-s-portfolio/blob/main/src/Assets/Pratik_Shastrakar_Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
