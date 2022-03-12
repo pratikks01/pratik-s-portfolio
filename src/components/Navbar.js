f65727bc4ecc425b7e14f0da5f0f89faf944ec79
Pratik Shastrakar <48235970+pstricks01@users.noreply.github.com>
2022-03-12 21:35:52 +0530

    Resume Updated 📃

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,71 +9,71 @@ import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
 
 import {
-  AiOutlineHome,
-  AiOutlineFundProjectionScreen,
-  AiOutlineUser,
+	AiOutlineHome,
+	AiOutlineFundProjectionScreen,
+	AiOutlineUser,
 } from "react-icons/ai";
 
 import { CgFileDocument } from "react-icons/cg";
 
 function NavBar() {
-  const [expand, updateExpanded] = useState(false);
-  const [navColour, updateNavbar] = useState(false);
+	const [expand, updateExpanded] = useState(false);
+	const [navColour, updateNavbar] = useState(false);
 
-  function scrollHandler() {
-    if (window.scrollY >= 20) {
-      updateNavbar(true);
-    } else {
-      updateNavbar(false);
-    }
-  }
+	function scrollHandler() {
+		if (window.scrollY >= 20) {
+			updateNavbar(true);
+		} else {
+			updateNavbar(false);
+		}
+	}
 
-  window.addEventListener("scroll", scrollHandler);
+	window.addEventListener("scroll", scrollHandler);
 
-  return (
-    <Navbar
-      expanded={expand}
-      fixed="top"
-      expand="md"
-      className={navColour ? "sticky" : "navbar"}
-    >
-      <Container>
-        <Navbar.Brand href="/">
-          {/* <img src={logo} className="img-fluid logo" alt="brand" /> */}
-          <h1>Pratik Shastrakar</h1>
-        </Navbar.Brand>
+	return (
+		<Navbar
+			expanded={expand}
+			fixed="top"
+			expand="md"
+			className={navColour ? "sticky" : "navbar"}
+		>
+			<Container>
+				<Navbar.Brand href="/">
+					{/* <img src={logo} className="img-fluid logo" alt="brand" /> */}
+					<h1>Pratik Shastrakar</h1>
+				</Navbar.Brand>
 
-        <Navbar.Toggle
-          aria-controls="responsive-navbar-nav"
-          onClick={() => {
-            updateExpanded(expand ? false : "expanded");
-          }}
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ml-auto" defaultActiveKey="#home">
-            <Nav.Item>
-              <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
-                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
-              </Nav.Link>
-            </Nav.Item>
+				<Navbar.Toggle
+					aria-controls="responsive-navbar-nav"
+					onClick={() => {
+						updateExpanded(expand ? false : "expanded");
+					}}
+				>
+					<span></span>
+					<span></span>
+					<span></span>
+				</Navbar.Toggle>
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="ml-auto" defaultActiveKey="#home">
+						<Nav.Item>
+							<Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
+								<AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+							</Nav.Link>
+						</Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/project"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineFundProjectionScreen
-                  style={{ marginBottom: "2px" }}
-                />{" "}
-                Projects
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
+						<Nav.Item>
+							<Nav.Link
+								as={Link}
+								to="/project"
+								onClick={() => updateExpanded(false)}
+							>
+								<AiOutlineFundProjectionScreen
+									style={{ marginBottom: "2px" }}
+								/>{" "}
+								Projects
+							</Nav.Link>
+						</Nav.Item>
+						{/* <Nav.Item>
               <Nav.Link
                 as={Link}
                 to="/resume"
@@ -81,22 +81,22 @@ function NavBar() {
               >
                 <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
               </Nav.Link>
-            </Nav.Item>
+            </Nav.Item> */}
 
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/about"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineUser style={{ marginBottom: "2px" }} /> About
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+						<Nav.Item>
+							<Nav.Link
+								as={Link}
+								to="/about"
+								onClick={() => updateExpanded(false)}
+							>
+								<AiOutlineUser style={{ marginBottom: "2px" }} /> About
+							</Nav.Link>
+						</Nav.Item>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
 }
 
 export default NavBar;
