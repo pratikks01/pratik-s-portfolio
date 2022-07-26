4ec48016ad949e8a8e03daaa25ea948751a1e0be
Pratik Shastrakar <48235970+pstricks01@users.noreply.github.com>
2022-07-26 18:41:48 +0530

    Grad Year updated

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -3,36 +3,36 @@ import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
 
 function AboutCard() {
-  return (
-    <Card className="quote-card-view">
-      <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-            Hello there!, I'm <span className="purple">Pratik Shastrakar </span>
-            from <span className="purple"> Nagpur, MH, India.</span>
-            <br />I am pursuing B.E. from Shri Ramdeobaba College of Engineering
-            and Management, Nagpur in Electronics Engineering and currently I am
-            in my III year. My Intrests are Data Structure and Algorithms and
-            Web Development.
-            <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
-          </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Photography 📸
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Making Videos 🎥
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Playing around with Electronics ⚡
-            </li>
-          </ul>
-        </blockquote>
-      </Card.Body>
-    </Card>
-  );
+	return (
+		<Card className="quote-card-view">
+			<Card.Body>
+				<blockquote className="blockquote mb-0">
+					<p style={{ textAlign: "justify" }}>
+						Hello there!, I'm <span className="purple">Pratik Shastrakar </span>
+						from <span className="purple"> Nagpur, MH, India.</span>
+						<br />I am pursuing B.E. from Shri Ramdeobaba College of Engineering
+						and Management, Nagpur in Electronics Engineering and currently I am
+						in my IV year. My Intrests are Data Structure and Algorithms and Web
+						Development.
+						<br />
+						<br />
+						Apart from coding, some other activities that I love to do!
+					</p>
+					<ul>
+						<li className="about-activity">
+							<ImPointRight /> Photography 📸
+						</li>
+						<li className="about-activity">
+							<ImPointRight /> Making Videos 🎥
+						</li>
+						<li className="about-activity">
+							<ImPointRight /> Playing around with Electronics ⚡
+						</li>
+					</ul>
+				</blockquote>
+			</Card.Body>
+		</Card>
+	);
 }
 
 export default AboutCard;
